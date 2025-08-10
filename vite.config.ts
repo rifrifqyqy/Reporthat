d9fbafcf2f24217ae08b4890751f3044c6200036
[rifrifqyqy] <rifqyh22@gmail.com>
2025-08-10 13:27:17 +0700

    chore: remove server configuration local

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		port: 3000,
-		host: '0.0.0.0'
-	}
+	plugins: [tailwindcss(), sveltekit()]
 });
